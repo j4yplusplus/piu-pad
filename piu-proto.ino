fb32142730e1d2c96ff40776b9531a6561ab2a01
J++ <j4yplusplus@gmail.com>
2026-08-12 10:42:38 -0500

    Added and tested debouncing.

--- piu-proto.ino
+++ piu-proto.ino
@@ -1,20 +1,33 @@
 #include <Keyboard.h>
 
-bool testMode = false;
+bool testMode = true;
 
-// Will be utilized if debouncing is needed. For now, redundant.
+// Amount of time needed for reading to be accepted as stable
+const unsigned long debounceDelay = 20;
+// For easier testing
+int count = 0;
+
+// Stable debounced states
 int topLeft = HIGH;
 int center = HIGH;
 int topRight = HIGH;
 int bottomLeft = HIGH;
 int bottomRight = HIGH;
 
-// Prev states
+// Previous raw electrical readings
 int topLeftPrev = HIGH;
 int centerPrev = HIGH;
 int topRightPrev = HIGH;
 int bottomLeftPrev = HIGH;
 int bottomRightPrev = HIGH;
+
+// Time when each reading was last changed
+unsigned long timerTL = 0;
+unsigned long timerC = 0;
+unsigned long timerTR = 0;
+unsigned long timerBL = 0;
+unsigned long timerBR = 0;
+
 
 void setup() {
   pinMode(2, INPUT_PULLUP);
@@ -27,40 +40,55 @@ void setup() {
   Keyboard.begin();
 }
 
+
 void loop() {
 
-  detectPress(2, 'w', topLeft, topLeftPrev);
-  detectPress(3, 's', center, centerPrev);
-  detectPress(4, 'd', topRight, topRightPrev);
-  detectPress(5, 'a', bottomLeft, bottomLeftPrev);
-  detectPress(6, 'x', bottomRight, bottomRightPrev);
+  handlePanel(2, 'w', topLeft, topLeftPrev, timerTL);
+  handlePanel(3, 's', center, centerPrev, timerC);
+  handlePanel(4, 'd', topRight, topRightPrev, timerTR);
+  handlePanel(5, 'a', bottomLeft, bottomLeftPrev, timerBL);
+  handlePanel(6, 'x', bottomRight, bottomRightPrev, timerBR);
 }
 
-void detectPress (int pin, char key, int &state, int &statePrev) {
-  state = digitalRead(pin);
 
-  if (state != statePrev) {
+void handlePanel (int pin, char key, int &stableState, int &prevReading, unsigned long &debounceTimer) {
+  int curReading = digitalRead(pin);
 
-    if (testMode == true) {
+  // If the current raw electrical reading is different from the previous raw reading
+  if (curReading != prevReading) {
+    debounceTimer = millis();
+    prevReading = curReading;
+  }
 
-      Serial.print("Pin ");
-      Serial.print(pin);
-      Serial.print(" changed to: ");
-      Serial.print(state == LOW ? "PRESSED" : "RELEASED");
-      Serial.print(" at ");
-      Serial.print(micros());
-      Serial.println(" us");
+  // If the current raw reading has been stable for longer than the debounce delay
+  if (millis() - debounceTimer >= debounceDelay) {
 
-    } else {
+    // Check if the current stable state is different from the last stable state
+    if (curReading != stableState) {
+      stableState = curReading;
 
-      if (state == LOW) {
-        Keyboard.press(key);
+      if (testMode) {
+
+        Serial.print("Pin ");
+        Serial.print(pin);
+        Serial.print(" changed to: ");
+        Serial.print(stableState == LOW ? "PRESSED" : "RELEASED");
+        Serial.print(" at ");
+        Serial.print(micros());
+        Serial.println(" us ");
+        Serial.println(count);
+
+        if (stableState == LOW) {count++;}
+
       } else {
-        Keyboard.release(key);
+
+        if (stableState == LOW) {
+          Keyboard.press(key);
+        } else {
+          Keyboard.release(key);
+        }
+
       }
-
     }
-
-    statePrev = state;
   }
 }
