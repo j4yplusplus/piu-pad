8bf40e55025d410635ada7303990ca1f2d8e0723
J++ <j4yplusplus@gmail.com>
2026-08-12 13:22:16 -0500

    Made panel struct and cleaned up comments.

--- piu-proto.ino
+++ piu-proto.ino
@@ -1,88 +1,99 @@
 #include <Keyboard.h>
 
 
+
+// ==========================================
+// Global Variables
+// ==========================================
+
 enum Mode {
   GAMEPLAY,
   CONTROLLED_TEST
 };
-
 
 // Gameplay is default mode
 Mode curMode = GAMEPLAY;
 
 String cmd = "";
 
-
 // Amount of time needed for reading to be accepted as stable
 const unsigned long debounceDelay = 20;
 
-// Stable debounced states
-int topLeft = HIGH;
-int center = HIGH;
-int topRight = HIGH;
-int bottomLeft = HIGH;
-int bottomRight = HIGH;
 
-// Previous raw readings
-int topLeftPrev = HIGH;
-int centerPrev = HIGH;
-int topRightPrev = HIGH;
-int bottomLeftPrev = HIGH;
-int bottomRightPrev = HIGH;
 
-// Time when each reading was last changed
-unsigned long timerTL = 0;
-unsigned long timerC = 0;
-unsigned long timerTR = 0;
-unsigned long timerBL = 0;
-unsigned long timerBR = 0;
+// ==========================================
+// Panels
+// ==========================================
 
+struct Panel {
+  int pin;
+  char key;
+  int stableState;
+  int prevReading;
+  unsigned long debounceTimer;
+};
+
+Panel topLeft = {2, 'w', HIGH, HIGH, 0};
+Panel center = {3, 's', HIGH, HIGH, 0};
+Panel topRight = {4, 'd', HIGH, HIGH, 0};
+Panel bottomLeft = {5, 'a', HIGH, HIGH, 0};
+Panel bottomRight = {6, 'x', HIGH, HIGH, 0};
+
+
+
+// ==========================================
+// Main Setup and Loop
+// ==========================================
 
 void setup() {
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
+  pinMode(topLeft.pin, INPUT_PULLUP);
+  pinMode(center.pin, INPUT_PULLUP);
+  pinMode(topRight.pin, INPUT_PULLUP);
+  pinMode(bottomLeft.pin, INPUT_PULLUP);
+  pinMode(bottomRight.pin, INPUT_PULLUP);
 
   Serial.begin(115200);
   Keyboard.begin();
 }
 
-
 void loop() {
 
   readSerialInput();
 
-  handlePanel(2, 'w', topLeft, topLeftPrev, timerTL);
-  handlePanel(3, 's', center, centerPrev, timerC);
-  handlePanel(4, 'd', topRight, topRightPrev, timerTR);
-  handlePanel(5, 'a', bottomLeft, bottomLeftPrev, timerBL);
-  handlePanel(6, 'x', bottomRight, bottomRightPrev, timerBR);
+  handlePanel(topLeft);
+  handlePanel(center);
+  handlePanel(topRight);
+  handlePanel(bottomLeft);
+  handlePanel(bottomRight);
 }
 
 
-void handlePanel (int pin, char key, int &stableState, int &prevReading, unsigned long &debounceTimer) {
-  int curReading = digitalRead(pin);
+
+// ==========================================
+// Panel Input Handling
+// ==========================================
+
+void handlePanel (Panel &panel) {
+  int curReading = digitalRead(panel.pin);
 
   // If the current raw electrical reading is different from the previous raw reading
-  if (curReading != prevReading) {
-    debounceTimer = millis();
-    prevReading = curReading;
+  if (curReading != panel.prevReading) {
+    panel.debounceTimer = millis();
+    panel.prevReading = curReading;
   }
 
   // If the current raw reading has been stable for longer than the debounce delay
-  if (millis() - debounceTimer >= debounceDelay) {
+  if (millis() - panel.debounceTimer >= debounceDelay) {
 
     // Check if the current stable state is different from the last stable state
-    if (curReading != stableState) {
-      stableState = curReading;
+    if (curReading != panel.stableState) {
+      panel.stableState = curReading;
 
       if (curMode == GAMEPLAY) {
-        if (stableState == LOW) {
-          Keyboard.press(key);
+        if (panel.stableState == LOW) {
+          Keyboard.press(panel.key);
         } else {
-          Keyboard.release(key);
+          Keyboard.release(panel.key);
         }
       }
 
@@ -92,6 +103,11 @@ void handlePanel (int pin, char key, int &stableState, int &prevReading, unsigne
     }
   }
 }
+
+
+// ==========================================
+// Controlled Test Mode Functions
+// ==========================================
 
 void modeCT() {
   Serial.println();
@@ -114,15 +130,21 @@ void modeCT() {
   Serial.println();
 }
 
-void displayPad(String color, String position, int state) {
+void displayPad(String color, String position, Panel &panel) {
   Serial.print("[");
-  if (state == LOW) {
+  if (panel.stableState == LOW) {
     Serial.print(color);
     Serial.print(": ");
     Serial.print(position);
   }
   Serial.print("] ");
 }
+
+
+
+// ==========================================
+// Serial Input Handling
+// ==========================================
 
 void readSerialInput() {
   while (Serial.available() > 0) {
@@ -142,19 +164,18 @@ void readSerialInput() {
         Serial.println();
         modeCT();
       }
-
       else if (cmd == "MODE GAME") {
         curMode = GAMEPLAY;
         Serial.println();
         Serial.println("GAMEPLAY MODE");
         Serial.println();
       }
-
       else {
         Serial.print("Unknown command: ");
         Serial.println(cmd);
       }
 
+      cmd = "";
     }
 
     else if (incoming != '\r') {
